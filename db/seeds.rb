2f0eeac011fc0ad714439a75fb5f8fc13850733d
Nicole Pupillo <nicole.pupillo@gmail.com>
2015-02-09 09:51:52 -0500

    Updated seeds.db

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,12 +19,16 @@ cat_maint = Category.create!(name: 'maintenance')
 cat_repair = Category.create!(name: 'repair')
 
 #Create events for roxanne
-roxanne.events.create(name: 'oil change', category: cat_maint, vendor: vend_squantum, cost: 65.00, event_date: '2014-11-05', due_date: '2014-11-10', notes: 'paid extra for synthetic oil')
+roxanne.events.create!(name: 'oil change', category: cat_maint, vendor: vend_squantum, cost: 65.00, event_date: '2014-11-05', due_date: '2014-11-10', notes: 'paid extra for synthetic oil')
 
-roxanne.events.create(name: 'flat tire', category: cat_repair, vendor: vend_bagnell, cost: 90.00, event_date: '2015-01-15', due_date: '2015-01-15', notes: 'free tire rotation')
+roxanne.events.create!(name: 'flat tire', category: cat_repair, vendor: vend_bagnell, cost: 90.00, event_date: '2015-01-15', due_date: '2015-01-15', notes: 'free tire rotation')
 
 #Create events for beigesavage
-beigesav.events.create(name: 'fuel pump', category: cat_repair, vendor: vend_squantum, cost: 100.00, event_date: '2014-10-25', due_date: '2014-10-25')
+beigesav.events.create!(name: 'fuel pump', category: cat_repair, vendor: vend_squantum, cost: 100.00, event_date: '2014-10-25', due_date: '2014-10-25')
+
+beigesav.events.create!(name: 'air filter', category: cat_repair, vendor: vend_squantum, cost: 20.00, event_date: '2015-02-06', due_date: '2015-02-06')
 
 #Create events for junker1
-junker1.events.create(name: 'hit a wall', category: cat_repair, vendor: vend_squantum, cost: 300.00, event_date: '2014-09-01', due_date: '2014-09-01')
+junker1.events.create!(name: 'hit a wall', category: cat_repair, vendor: vend_squantum, cost: 300.00, event_date: '2014-09-01', due_date: '2014-09-01')
+
+junker1.events.create!(name: 'new roof', category: cat_repair, vendor: vend_bagnell, cost: 500.00, event_date: '2014-03-20', due_date: '2014-03-20')
