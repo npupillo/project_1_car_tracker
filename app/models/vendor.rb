5f3b5ee3668a67a87ab14ed837ce8d29eac39058
Nicole Pupillo <nicole.pupillo@gmail.com>
2015-02-09 11:41:49 -0500

    Experiment: added accepts_nested_attributes_for to vendor model, trying to get vendor name (not just vendor_id) to show up in individual events on vehicle show page

--- app/models/vendor.rb
+++ app/models/vendor.rb
@@ -2,5 +2,7 @@ class Vendor < ActiveRecord::Base
   belongs_to :events
   has_many :vehicles, through: :events
 
+  accepts_nested_attributes_for :events, :vehicles
+
   validates :name, presence: true
 end
