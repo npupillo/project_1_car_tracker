a63a653d128ecd6e772b3462b11fadb520109b09
Nicole Pupillo <nicole.pupillo@gmail.com>
2015-02-11 19:33:35 -0500

    Updated counters on vehicles index page

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -6,7 +6,6 @@ class EventsController < ApplicationController
   #set the event instance for some actions
   before_action :set_event, only: [:show, :edit, :update, :destroy]
   def index
-    # @vehicle = Vehicle.find(params[:vehicle_id])
     @events = @vehicle.events
   end
 
@@ -19,7 +18,7 @@ class EventsController < ApplicationController
 
   def create
     @event = @vehicle.events.new(event_params)
-#binding.pry
+# binding.pry
     #GET the vendor_id & category_id from the select dropdowns in the form
     @event.vendor = Vendor.find(params[:event][:vendor_id])
     @event.category = Category.find(params[:event][:category_id])
@@ -61,7 +60,9 @@ private
   # Allow params for event and for vendor & category
   def event_params
     #Also whitelist the vendor & category attributes:
-    params.require(:event).permit(:name, :vehicle, :vendor, :category, :cost, :event_date, :due_date, :notes, vendors_attributes: [:id, :name, :address, :phone, :website, :hours, :notes], categories_attributes: [:id, :name])
+    params.require(:event).permit(:name, :vehicle, :vendor, :category, :cost, :event_date, :due_date, :notes,
+      vendors_attributes: [:id, :name, :address, :phone, :website, :hours, :notes],
+      categories_attributes: [:id, :name])
   end
 
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -3,6 +3,22 @@ class Event < ActiveRecord::Base
   belongs_to :vendor
   belongs_to :category
 
+  def overdue?
+    if self.due_date.past? && self.event_date.future?
+      return 'YES'
+    else
+      return 'NO'
+    end
+  end
+
+  def future_event
+    if self.due_date.future? && self.event_date.future?
+      return 'YES'
+    else
+      return 'NO'
+    end
+  end
+
   validates :name, presence: true
 end
 
